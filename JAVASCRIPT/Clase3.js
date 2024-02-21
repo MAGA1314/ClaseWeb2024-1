4431bb6a92faf686ac27b985874097d3dc80223c
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-02-21 12:24:14 -0500

    clase3

---
+++ JAVASCRIPT/Clase3.js
@@ -0,0 +1,9 @@
+//Declarar una variable
+let nombreUsuario;
+let user = "Miguel";
+
+console.log(nombreUsuario)
+console.log(user);
+
+//operador ternario
+user == "Pedro"? console.log("Verdadrero"):console.log("Falso");
